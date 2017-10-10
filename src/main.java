44c3dae718e1ca6a45c59c2cdba7f30893678275
Kery2004 <32651131+Kery2004@users.noreply.github.com>
2017-10-10 22:14:47 +0200

    Update
    almost finally done

--- src/main.java
+++ src/main.java
@@ -44,9 +44,20 @@ public class main {
 		}
 	}
 
+	public static void loopthroughDrinks(ArrayList<Drinks> arrayDrinks) {
+		int loopchoice = 1;
+		for (Drinks o : arrayDrinks) { // loop through all Cuisines
+			System.out.println(loopchoice + "." + o);
+			loopchoice++;
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int sumOrder = 0;
+		ArrayList<Cuisines> lista = new ArrayList<Cuisines>(); // list of Cuisines
+		boolean icecubs = false, lemon = false;
+		String namedish = "", namedesert = "", namedrink = "";
 		// First data from user below:
 		System.out.println("Do you want order lunch or drink?");
 		ArrayList<String> helplist = new ArrayList<String>(); // list of Cuisines
@@ -71,26 +82,103 @@ public class main {
 			switch (readfromuser(x)) {
 			case 1:
 				System.out.println("here's the list of Poland Cuisines");
-				ArrayList<Cuisines> lista = new ArrayList<Cuisines>(); // list of Cuisines
 				lista.add(new Poland("Schabowy", 15));
 				lista.add(new Poland("Golonka", 16));
 				lista.add(new Poland("¯urek", 11));
 				loopthroughCuisines(lista);
-
-				int sumOrder = lista.get(readfromuser(x)).getPrice();
+				x = 0;
+				x = lista.size();
+				int xyu = readfromuser(x);
+				namedish = lista.get(xyu - 1).getName();
+				sumOrder = lista.get(xyu - 1).getPrice();
+				lista.clear();
+				System.out.println("here's the list of Poland desert");
+				lista.add(new Poland("ciasto jablecznikowe", 15));
+				lista.add(new Poland("ciasto takie", 16));
+				lista.add(new Poland("ciasto inne", 11));
+				loopthroughCuisines(lista);
+				x = 0;
+				x = lista.size();
+				xyu = readfromuser(x);
+				namedesert = lista.get(xyu - 1).getName();
+				sumOrder += lista.get(xyu - 1).getPrice();
 				break;
 			case 2:
 				System.out.println("here's the list of Mexican Cuisines");
+				lista.add(new Mexican("Chilaquiles", 15));
+				lista.add(new Mexican("Pozole", 16));
+				lista.add(new Mexican("Tacos al pastor", 11));
+				loopthroughCuisines(lista);
+				x = 0;
+				x = lista.size();
+				xyu = readfromuser(x);
+				namedish = lista.get(xyu - 1).getName();
+				sumOrder += lista.get(xyu - 1).getPrice();
+				lista.clear();
+				System.out.println("here's the list of Mexican desert");
+				lista.add(new Poland("ciasto jablecznikowe", 15));
+				lista.add(new Poland("ciasto takie", 16));
+				lista.add(new Poland("ciasto inne", 11));
+				loopthroughCuisines(lista);
+				x = 0;
+				x = lista.size();
+				xyu = readfromuser(x);
+				namedesert = lista.get(xyu - 1).getName();
+				sumOrder += lista.get(xyu - 1).getPrice();
 				break;
 			case 3:
 				System.out.println("here's the list of Italiano Cuisines");
-				break;
-			}
-			// Second data from user
-			// scan.reset(); // cleaning input
-			// check = false; // assign start value for check
-			// schoice = scan.nextInt();
+				lista.add(new Italiano("Schabowy", 15));
+				lista.add(new Italiano("Golonka", 16));
+				lista.add(new Italiano("¯urek", 11));
+				loopthroughCuisines(lista);
+				x = 0;
+				x = lista.size();
+				xyu = readfromuser(x);
+				namedish = lista.get(xyu - 1).getName();
+				sumOrder += lista.get(xyu - 1).getPrice();
+				lista.clear();
+				System.out.println("here's the list of Italiano desert");
+				lista.add(new Poland("ciasto jablecznikowe", 15));
+				lista.add(new Poland("ciasto takie", 16));
+				lista.add(new Poland("ciasto inne", 11));
+				loopthroughCuisines(lista);
 
+				x = 0;
+				x = lista.size();
+				xyu = readfromuser(x);
+				namedesert = lista.get(xyu - 1).getName();
+				sumOrder += lista.get(xyu - 1).getPrice();
+				break;
+
+			}
+			System.out.println("Your order is " + namedish + " and " + namedesert + " will cost " + sumOrder + "$");
+			break;
+		case 2:
+			ArrayList<Drinks> drinklist = new ArrayList<Drinks>(); // list of Cuisines
+			System.out.println("here's the list of drinks");
+			drinklist.add(new Drinks("Schabowy", 15));
+			drinklist.add(new Drinks("Golonka", 16));
+			drinklist.add(new Drinks("¯urek", 11));
+			loopthroughDrinks(drinklist);
+			int ux = 0;
+			ux = drinklist.size();
+			int xyu1 = readfromuser(ux);
+			namedish = drinklist.get(xyu1 - 1).getName();
+			sumOrder = drinklist.get(xyu1 - 1).getPrice();
+			System.out.println("With ice cubs?\n" + "1.Yes\n" + "2. No");
+			icecubs = (readfromuser(2) == 1) ? true : false;
+			System.out.println("how about lemon?\n" + "1.Yes\n" + "2. No");
+			lemon = (readfromuser(2) == 1) ? true : false;
+			if (icecubs == true && lemon == true) {
+				System.out.println(
+						"Your order is " + namedish + " with icecubs and lemons " + " will cost " + sumOrder + "$");
+			} else if (icecubs == true) {
+				System.out.println("Your order is " + namedish + " with icecubs " + " will cost " + sumOrder + "$");
+			} else if (lemon == true) {
+				System.out.println("Your order is " + namedish + " with lemons " + " will cost " + sumOrder + "$");
+			} else
+				System.out.println("Your order is " + namedish + " will cost " + sumOrder + "$");
 			break;
 
 		}
